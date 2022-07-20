c92f5450ff0d6b4bbe2803c19cae161dc1f973ff
Francisco Lima <francisco.lima@liferay.com>
2022-07-20 11:29:01 -0300

    LPS-157305 buildREST

--- modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/resource/v1_0/MessageBoardThreadResource.java
+++ modules/apps/headless/headless-delivery/headless-delivery-api/src/main/java/com/liferay/headless/delivery/resource/v1_0/MessageBoardThreadResource.java
@@ -76,6 +76,13 @@ public interface MessageBoardThreadResource {
 			Long messageBoardSectionId, String callbackURL, Object object)
 		throws Exception;
 
+	public Page<MessageBoardThread>
+			getMessageBoardSectionFilteredMessageBoardThreadsPage(
+				Long messageBoardSectionId, String search, String tag,
+				com.liferay.portal.vulcan.aggregation.Aggregation aggregation,
+				Filter filter, Pagination pagination, Sort[] sorts)
+		throws Exception;
+
 	public Page<MessageBoardThread> getMessageBoardThreadsRankedPage(
 			Date dateCreated, Date dateModified, Long messageBoardSectionId,
 			Pagination pagination, Sort[] sorts)
