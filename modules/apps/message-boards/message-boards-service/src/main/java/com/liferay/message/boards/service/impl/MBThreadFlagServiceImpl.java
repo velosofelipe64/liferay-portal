4cdadcfb26ff722e55b81f79171a6e76542778fa
Felipe Veloso <felipe.veloso@liferay.com>
2022-07-20 10:11:21 -0300

    LPS-152322 settings MBThreadFlag Entity

---
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBThreadFlagServiceImpl.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.service.impl;
+
+import com.liferay.message.boards.model.MBThread;
+import com.liferay.message.boards.model.MBThreadFlag;
+import com.liferay.message.boards.service.base.MBThreadFlagServiceBaseImpl;
+import com.liferay.portal.aop.AopService;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(
+	property = {
+		"json.web.service.context.name=mb",
+		"json.web.service.context.path=MBThreadFlag"
+	},
+	service = AopService.class
+)
+public class MBThreadFlagServiceImpl extends MBThreadFlagServiceBaseImpl {
+
+
+	@Override
+	public MBThreadFlag addThreadFlag(
+		MBThread thread, ServiceContext serviceContext)
+		throws PortalException {
+
+		return mbThreadFlagLocalService.addThreadFlag(
+			getUserId(),thread,serviceContext);
+	}
+
+
+
+}
