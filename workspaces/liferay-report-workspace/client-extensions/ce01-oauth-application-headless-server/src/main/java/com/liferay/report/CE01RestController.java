b4279c161c604844a1d606e8c73f0811317481f5
Nilton Vieira <nilton.vieira@liferay.com>
2023-02-21 15:47:41 -0300

    generatePDF

--- workspaces/liferay-report-workspace/client-extensions/ce01-oauth-application-headless-server/src/main/java/com/liferay/report/CE01RestController.java
+++ workspaces/liferay-report-workspace/client-extensions/ce01-oauth-application-headless-server/src/main/java/com/liferay/report/CE01RestController.java
@@ -14,14 +14,33 @@
 
 package com.liferay.report;
 
+import com.liferay.report.dto.Template;
+
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONObject;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,6 +48,78 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class CE01RestController {
+
+	@PostMapping("/generatepdf")
+	public ResponseEntity<?> generatePDF(@RequestBody Template template)
+		throws Exception {
+
+		System.out.println(template.getName());
+
+		File tempOdtFinal = File.createTempFile("reportODTFinal", ".odt");
+
+		generateReport(template, tempOdtFinal);
+
+		String tmpdir = System.getProperty("java.io.tmpdir");
+
+		ProcessBuilder processBuilder = new ProcessBuilder(
+			_libreOfficePath, "--headless", "--convert-to",
+			"pdf:writer_pdf_Export", "--outdir", tmpdir,
+			tempOdtFinal.getAbsolutePath());
+
+		Process process = processBuilder.start();
+
+		process.waitFor();
+
+		String fileName = tempOdtFinal.getName();
+
+		File pdfFile = new File(tmpdir, fileName.split(".odt")[0] + ".pdf");
+
+		FileInputStream fileInputStream = null;
+		InputStreamResource inputStreamResource = null;
+		HttpHeaders httpHeaders = new HttpHeaders();
+
+		try {
+			httpHeaders.setContentLength(pdfFile.length());
+
+			fileInputStream = new FileInputStream(pdfFile);
+
+			inputStreamResource = new InputStreamResource(fileInputStream);
+		}
+		catch (FileNotFoundException fileNotFoundException) {
+			return new ResponseEntity<>(
+				"Failed to generate response from PDF file " +
+					fileNotFoundException.toString(),
+				HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		tempOdtFinal.delete();
+		pdfFile.delete();
+		System.out.println("End :: [generatePDF]");
+
+		return new ResponseEntity<>(
+			inputStreamResource, httpHeaders, HttpStatus.OK);
+	}
+
+	public void generateReport(Template template, File file) throws Exception {
+		InputStream inputStream = CE01RestController.class.getResourceAsStream(
+			"/template-report1.odt");
+
+		XDocReportRegistry xDocReportRegistry =
+			XDocReportRegistry.getRegistry();
+
+		IXDocReport report = xDocReportRegistry.loadReport(
+			inputStream, TemplateEngineKind.Freemarker);
+
+		IContext context = report.createContext();
+
+		context.put("template", template);
+
+		OutputStream outputStream = new FileOutputStream(file);
+
+		report.process(context, outputStream);
+
+		outputStream.close();
+	}
 
 	@GetMapping("/")
 	public ResponseEntity<String> home() {
@@ -44,5 +135,8 @@ public class CE01RestController {
 	}
 
 	private static final Log _log = LogFactory.getLog(CE01RestController.class);
+
+	@Value("${libreoffice.path}")
+	private String _libreOfficePath;
 
 }
